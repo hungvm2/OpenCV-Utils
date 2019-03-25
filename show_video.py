2fb9852a574a86b06f3f637697aa08787453160c
HungVM2 <lolo.aquarius@gmail.com>
2019-03-25 16:26:16 +0700

    add some new notes

--- show_video.py
+++ show_video.py
@@ -1,6 +1,6 @@
 import cv2
 
-vidcap = cv2.VideoCapture('rtsp://admin:Technology@@169.254.46.181/h265/ch1/main/av_stream')
+vidcap = cv2.VideoCapture('rtsp://test:abcd1234@10.84.130.40/h265/ch8/main/av_stream')
 vidcap.set(3,640) #set frame width
 vidcap.set(4,480) #set frame height
 # vidcap = cv2.VideoCapture(args.get('video'))
